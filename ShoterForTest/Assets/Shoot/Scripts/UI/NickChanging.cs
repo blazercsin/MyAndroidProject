e0b4278f8251bd2c9a33d92d34b6d166d2438d6f
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:06:51 +0300

    Create NickChanging.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/UI/NickChanging.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NickChanging : MonoBehaviour
+{
+    public string nick;
+    public InputField ifs;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        nick = PlayerPrefs.GetString("nick");
+        ifs.text = nick;
+        if (nick == null)
+        {
+            PlayerPrefs.SetString("nick", "Player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ifs.text != nick)
+        {
+            nick = ifs.text;
+            PlayerPrefs.SetString("nick", nick);
+        }
+    }
+}
