780c10e0ccb9ff3f269c8de8d96769734f3a3b99
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:09:48 +0300

    Create AnimatioSync.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/Network/AnimatioSync.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatioSync : MonoBehaviour
+{
+    public PhotonView phe;
+    public float timer = 0.1f;
+    public AnimControl ac;
+    public movement mov;
+    // Start is called before the first frame update
+    void Start()
+    {
+        phe = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (phe.isMine)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                phe.RPC("RPCAnum", PhotonTargets.All, mov.isMove);
+                timer = 0.1f;
+            }
+        }
+        
+    }
+    [PunRPC]
+    public void RPCAnum(bool an)
+    {
+        ac.Move(an);
+    }
+}
