26df995945000c949eac1107ec9e459b18596615
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:12:12 +0300

    Create Destr.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/character/BattleSystem/Destr.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destr : MonoBehaviour
+{
+    public float time = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
