65f22c7c0071d3f76b6299144fbd688ed13bd81f
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:07:59 +0300

    Create GranadeShot.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/TestTrash/GranadeShot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GranadeShot : MonoBehaviour
+{
+    public GameObject pref;
+    public float force;
+    public bool shoot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (shoot)
+        {
+           GameObject go =  GameObject.Instantiate(pref, transform.position, Quaternion.identity) as GameObject;
+            go.GetComponent<Rigidbody>().AddForce(transform.forward * force);
+            shoot = false;
+
+        }
+        
+    }
+}
