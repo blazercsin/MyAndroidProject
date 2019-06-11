94565b2eced9568d0b11c74a9cd4509923150240
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:11:55 +0300

    Create BonusSpeed.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/character/BonusSpeed.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusSpeed : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timer = 10;
+    public bool bonus;
+    public movement mv;
+    public GameObject mc;
+    void Start()
+    {
+        mc = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bonus)
+        {
+            mc.GetComponent<InGameUI>().speedbar.gameObject.SetActive(true);
+            mc.GetComponent<InGameUI>().speedbar.fillAmount = timer / 10;
+            timer -= Time.deltaTime;
+            mv.speed = 8;
+            Debug.Log(" I`m FAST AS FUCK BOI!!!");
+            if (timer <= 0)
+            {
+                mc.GetComponent<InGameUI>().speedbar.gameObject.SetActive(false);
+                mv.speed = 5;
+                timer = 10;
+                bonus = false;
+            }
+        }
+    }
+}
