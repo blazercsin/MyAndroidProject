863838fcc0d1cd4df596a632b263c4dd049d82a9
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:11:19 +0300

    Create TargetMark.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/character/Enemy/TargetMark.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetMark : MonoBehaviour
+{
+    public bool InTarget;
+    public GameObject targmar;
+    public GameObject MainCameraa;
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainCameraa = GameObject.Find("Main Camera");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (InTarget)
+        {
+            MainCameraa.GetComponent<HPUI>().target = this.transform;
+            targmar.SetActive(true);
+        }
+        else
+        {
+
+            targmar.SetActive(false);
+        }
+    }
+}
