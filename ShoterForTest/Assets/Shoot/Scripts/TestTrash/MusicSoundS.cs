b2323b01ebb1ef2506aec4d704125bd19d09ec74
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:07:33 +0300

    Create MusicSoundS.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/TestTrash/MusicSoundS.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSoundS : MonoBehaviour
+{
+    public bool m;
+    public bool s;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (m)
+        {
+            int ms = PlayerPrefs.GetInt("Music");
+            if (ms == 1)
+            {
+                GetComponent<AudioSource>().volume = 0;
+            }
+            
+        }
+        if (s)
+        {
+            int ss = PlayerPrefs.GetInt("Sound");
+            if (ss == 1)
+            {
+                GetComponent<AudioSource>().volume = 0;
+            }
+        }
+
+    }
+
+   
+}
