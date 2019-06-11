ffc2a2e7e644778ab5b89b94892d2ad1d05d54b7
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:10:38 +0300

    Create SpawnPoints.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/character/SpawnPoints.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoints : MonoBehaviour
+{
+    public Transform[] spawnpoints;
+    public GameObject pl;
+    public int r;
+    public bool trys;
+    // Start is called before the first frame update
+    void Start()
+    {
+        trys = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (pl == null&&trys)
+        {
+            RandSpawn();
+            trys = false;
+        }
+    }
+    public void RandSpawn()
+    {
+        r = Random.RandomRange(0, spawnpoints.Length);
+        spawnpoints[r].GetComponent<PlayerInstant>().spawn = true;
+    }
+}
