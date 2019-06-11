9dcb52a2e15dac741091710016ae17812262dc8c
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:11:49 +0300

    Create CameraChar.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/character/CameraChar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraChar : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+    public CamScript cs;
+    public float speed = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cs = GetComponent<CamScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target != null)
+        {
+          //  cs.mp = target;
+            transform.position =Vector3.Lerp(transform.position, target.position + offset,speed);
+        }
+    }
+    public void FireClick()
+    {
+        if (target != null)
+        {
+          
+            target.GetComponent<ClosestTarget>().GetClosest();
+            target.GetComponent<GunController>().Shotgun();
+        }
+    }
+}
