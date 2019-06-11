11ec6599f428a6fcfe9b7a6fba4d7d1a2970f388
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:07:43 +0300

    Create grassshake.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/TestTrash/grassshake.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class grassshake : MonoBehaviour
+{
+    public Quaternion Rot;
+    public Quaternion startrot;
+
+    void Awake()
+    {
+        startrot = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Rot.z += Time.deltaTime*0.02f;
+
+        transform.rotation = Rot;
+       
+    }
+}
