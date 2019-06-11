ee51097af1f1dccaab9d55790f41d1ef5b1c12b6
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:09:04 +0300

    Create HPBAR.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/Network/HPBAR.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HPBAR : MonoBehaviour
+{
+    public Image bar;
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            if (GameObject.FindGameObjectWithTag("Player"))
+            {
+               // target = GameObject.FindGameObjectWithTag("Player") as GameObject;
+                 
+                
+
+            }
+        }
+    }
+}
