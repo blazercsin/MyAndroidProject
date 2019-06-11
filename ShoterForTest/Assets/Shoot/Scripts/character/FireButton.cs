2ab9ef353f8659f18e384c1e54899f8470deb51f
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:11:13 +0300

    Create FireButton.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/character/FireButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireButton : MonoBehaviour
+{
+  
+    public void SendFire()
+    {
+        Debug.Log("FireButtonON");
+    }
+    public void SendVector()
+    {
+        Debug.Log("chooseVector");
+    }
+}
