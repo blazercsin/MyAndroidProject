c552003e3771e65dca492ef5da9d7ac97df70d63
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:08:08 +0300

    Create FPS.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/TestTrash/FPS.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS : MonoBehaviour
+{
+    public Text fpst;
+    public float deltaTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+        float fps = 1.0f / deltaTime;
+        fpst.text = ("FPS: "+Mathf.Ceil(fps).ToString());
+    }
+}
