66e4cadaa5b207d175df5651e02607a2b97341bd
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:07:18 +0300

    Create water.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/TestTrash/water.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class water : MonoBehaviour
+{
+    public Renderer mat;
+    public float scrollSpeed = 0.2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+      
+        mat = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * scrollSpeed;
+        mat.material.SetTextureOffset("_MainTex", new Vector2(offset, offset));
+    }
+}
