0b05586188aedd19f911538bdd3edea9d70dc5a0
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:08:31 +0300

    Create SetPosition.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/Network/SetPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPosition : MonoBehaviour
+  
+{
+    public PhotonView phe;
+    // Start is called before the first frame update
+    void Start()
+    {
+    phe = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    [PunRPC]
+ void GetPosition()
+{
+
+}
+}
