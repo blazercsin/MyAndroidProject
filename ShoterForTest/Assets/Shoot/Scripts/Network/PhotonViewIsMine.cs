f2cf51cde1d2e188219862a2e35f4173ec29b660
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:08:43 +0300

    Create PhotonViewIsMine.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/Network/PhotonViewIsMine.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonViewIsMine : MonoBehaviour
+{
+    public PhotonView phe;
+    public movement mov;
+    public CharacterController cc;
+    public ClosestTarget ct;
+    public TargetMark tm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        phe = GetComponent<PhotonView>();
+
+        if (!phe.isMine)
+        {
+            gameObject.tag = "Enemy";
+            mov.enabled = false;
+           // cc.enabled = false;
+            ct.enabled = false;
+            tm.enabled = true;
+        }
+        else
+        {
+            mov.ss = GameObject.Find("FireButton").GetComponent<ShootStick>();
+            mov.MainCam = GameObject.FindGameObjectWithTag("MainCamera");
+            mov.MainCam.GetComponent<CameraChar>().target = this.gameObject.transform;
+            mov.ts = GameObject.Find("LeftStick").GetComponent<TouchStick>();
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
