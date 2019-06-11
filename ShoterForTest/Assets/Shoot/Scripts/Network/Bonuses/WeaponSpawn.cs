a410b4a40517e1b5d679342b15980886d1ca87fb
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:09:20 +0300

    Create WeaponSpawn.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/Network/Bonuses/WeaponSpawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSpawn : MonoBehaviour
+{
+    public GameObject go;
+    public float timer;
+    public int id;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (go == null)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                if (PhotonNetwork.isMasterClient)
+                {
+                    go = PhotonNetwork.Instantiate("Weapon"+id, transform.position, Quaternion.identity, 0) as GameObject;
+                }
+                timer = 10;
+            }
+        }
+    }
+}
