da32d60f7e9c9b6014f3efe550ab4e924e1c52a6
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:07:28 +0300

    Create PhotonDestroy.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/TestTrash/PhotonDestroy.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotonDestroy : MonoBehaviour
+{
+    public PhotonView phe;
+    public float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            if (phe.isMine)
+            {
+                PhotonNetwork.Destroy(this.gameObject);
+            }
+        }
+    }
+}
