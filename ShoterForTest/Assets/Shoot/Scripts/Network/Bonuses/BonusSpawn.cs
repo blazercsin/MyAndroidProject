6c05155517ab32fe2f6b8c9ea837ce21624af2e9
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:09:30 +0300

    Create BonusSpawn.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/Network/Bonuses/BonusSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusSpawn : MonoBehaviour
+{
+    public GameObject go;
+    public float timer ;
+    public int id;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (go == null)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                if (PhotonNetwork.isMasterClient)
+                {
+                    if (id == 0)
+                    {
+                        go = PhotonNetwork.Instantiate("Heal", transform.position, transform.rotation, 0) as GameObject;
+                    }
+                    if (id == 1)
+                    {
+                        go = PhotonNetwork.Instantiate("Speed", transform.position, transform.rotation, 0) as GameObject;
+                    }
+                }
+                timer = 5;
+            }
+        }
+    }
+}
