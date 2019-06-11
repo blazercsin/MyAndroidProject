9edee2a4a16857748549bff6db858bd53c1f8a5c
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:07:07 +0300

    Create ConnectButton.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/UI/ConnectButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConnectButton : MonoBehaviour
+{
+    public Text nameroom;
+    public Text countPlayers;
+    public string nameoftheroom;
+    public string countoftheplayers;
+
+
+    public void Start()
+    {
+        nameroom.text = nameoftheroom;
+        countPlayers.text = countoftheplayers + "/4";
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Clicksy()
+    {
+        PhotonNetwork.JoinRoom(nameoftheroom);
+    }
+    private void OnDisable()
+    {
+        Destroy(this.gameObject);
+    }
+}
