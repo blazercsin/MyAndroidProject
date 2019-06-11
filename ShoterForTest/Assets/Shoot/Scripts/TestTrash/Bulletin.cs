4299057d7e7e0edc15783a5635cdc8df9a85d0cb
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:08:17 +0300

    Create Bulletin.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/TestTrash/Bulletin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bulletin : MonoBehaviour
+{
+    public AudioClip[] ac;
+    public PhotonView phe;
+    public float timer = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int r = Random.Range(0, ac.Length);
+        GetComponent<AudioSource>().clip = ac[r];
+        GetComponent<AudioSource>().Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer <= 0)
+        {
+            if (GetComponent<PhotonView>().isMine)
+            {
+                PhotonNetwork.Destroy(this.gameObject);
+            }
+        }
+    }
+}
