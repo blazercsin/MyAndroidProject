40e5316c850aa3c8b08681c6b2fb0e4391e7f896
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-11 09:11:44 +0300

    Create CamScript.cs

---
+++ ShoterForTest/Assets/Shoot/Scripts/character/CamScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class CamScript : MonoBehaviour {
+    public float vobrTim;
+	public float tim_speed=0.4f;
+	public Vector3 spos;
+	
+	// Use this for initialization
+	void Start () {
+		spos = transform.position;
+	}
+
+	public void Vibro(float d){
+		vobrTim = d;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (vobrTim > 0) {
+			vobrTim-=Time.deltaTime*tim_speed;
+			transform.position+=new Vector3(Random.Range(-vobrTim,vobrTim),0,Random.Range(-vobrTim,vobrTim));
+		}
+		
+		}
+}
